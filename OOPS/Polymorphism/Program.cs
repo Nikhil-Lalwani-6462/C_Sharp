76c49b613e3e0f6972b8d8e335a1e610779ae3f0
Nikhil-Lalwani-6462 <nikhillalwani6462@gmail.com>
2025-10-29 11:26:33 -0700

    Overide Polymorphism Program added

--- OOPS/Polymorphism/Program.cs
+++ OOPS/Polymorphism/Program.cs
@@ -4,52 +4,75 @@ class Program
 {
     static void Main(String[] args)
     {
-        string ch;
+        string ch,new_ch;
         int no;
-        int days;
-        Vehicle ve = new Vehicle();
+
         Console.WriteLine("Enter Choice Car/Bike/Bus:");
         ch = Console.ReadLine();
+
         Console.WriteLine("Enter Number of day you want a vehicle for rent");
         no = Convert.ToInt32(Console.ReadLine());
 
-        days = ve.calculateRent(no, ch.ToLower());
-        Console.WriteLine("Rent of the vehicle for number of days:" +days);
+        Vehicle ve;
 
+        new_ch = ch.ToLower();
+        if (new_ch == "bike")
+        {
+            ve = new Bike();
+            ve.calculateRent(no);
+        }
+        else if (new_ch == "car")
+        {
+            ve = new Car();
+            ve.calculateRent(no);
+        }
+        else if (new_ch == "bus")
+        {
+            ve = new Bus();
+            ve.calculateRent(no);
 
-        
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice for rent");
+        }
     }
 }
 class Vehicle
 {
-    int BikeRent = 500;
-    int CarRent = 1000;
-    int BusRent = 2000;
-    int Rent = 0;
-
-    public int calculateRent(int days, string ch)
+    public virtual void calculateRent(int days)
     {
-        if (ch == "bike")
-        {
+        Console.WriteLine("Days for the vehicle for rent:"+days);
+    }
 
-            Rent = BikeRent * days;
+}
+class Bike : Vehicle
+{
+    int BikeRent = 500, Rent;
 
+    public override void calculateRent(int days)
+    {
+        Rent = days * BikeRent;
+        Console.WriteLine("Rent for the bike for" + days + ":" + Rent);
+    }
 
-        }
-        else if (ch == "car")
-        {
-
-            Rent = CarRent * days;
-
-        }
-        else if (ch == "bus")
-        {
-            Rent = BusRent * days;
-        }
-        else
-        {
-            Console.WriteLine("Invalid Vehicle Choice");
-        }
-        return Rent;
+}
+class Car : Vehicle
+{
+    int CarRent = 1000, Rent;
+    public override void calculateRent(int days)
+    {
+        Rent = days * CarRent;
+        Console.WriteLine("Rent for the Car for" + days + ":" + Rent);
     }
 }
+class Bus : Vehicle
+{
+    int BusRent = 2000,Rent;
+    public override void calculateRent(int days)
+    {
+        Rent = days * BusRent;
+        Console.WriteLine("Rent for the Bus for" + days + ":" + Rent);
+    }
+}
+
--- OOPS/Polymorphism/obj/Debug/net9.0/Polymorphism.AssemblyInfo.cs
+++ OOPS/Polymorphism/obj/Debug/net9.0/Polymorphism.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391baa2d8c8326d3c02c03bb3525ed8f64886b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
