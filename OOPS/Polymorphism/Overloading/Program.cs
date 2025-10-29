ea65081c63efad1ca15598d12eca910b22f88b3c
Nikhil-Lalwani-6462 <nikhillalwani6462@gmail.com>
2025-10-29 13:15:30 -0700

    Polymorphism program added

---
+++ OOPS/Polymorphism/Overloading/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int amount;
+        string account, note;
+        Bank ba = new Bank();
+        Console.Write("Enter amount to deposit in account");
+        amount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.Write("Enter Account type of the account:");
+        account = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.Write("Enter Note:");
+        note = Console.ReadLine();
+
+        ba.Deposit(amount);
+        ba.Deposit(amount, account);
+        ba.Deposit(amount, account, note);
+
+    }
+}
+class Bank
+{
+    public void Deposit(int amount)
+    {
+        Console.WriteLine("Amount deposited in the account:" + amount);
+    }
+    public void Deposit(int amount, string account)
+    {
+
+        Console.WriteLine(+amount + " deposited in " + account + "account");
+    }
+    public void Deposit(int amount, string account, string note)
+    {
+        Console.WriteLine(+amount + "deposited in " + account +"Note:"+note);
+    }
+}
+
+
